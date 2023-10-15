ed84fa6d9979f88b5a77546f75031ee9ead081b1
Santiago Rivera <santiagorvrcn@gmail.com>
2023-10-15 15:41:55 -0500

    Add filters

---
+++ FiltradorBusqueda/js/app.js
@@ -0,0 +1,168 @@
+import { peliculas } from "./base.js";
+
+
+
+/*[--SELECTS] --*/
+const years = document.querySelector('#Year');
+const generes = document.querySelector('#genres');
+const titles = document.querySelector('#names');
+const resultHtml = document.querySelector('.result');
+
+/*[--Years generate--] for options */
+const yearMovieMax = new Date().getFullYear() - 3;
+const yearMovieMin = yearMovieMax - 13;
+
+
+let objMovie = {
+    nameMovie: "",
+    nameGenere: "",
+    yearsMovie: "",
+
+}
+
+
+
+const createYears = () => {
+    for(let i = yearMovieMax; i >= yearMovieMin; i--){
+        generateOption(i, years)
+    }
+
+
+}
+
+
+const createMovies = () => {
+    for(let i = 0; i < peliculas.length; i++){
+        let movies = peliculas[i].nombre
+        generateOption(movies, titles)
+    }
+}
+
+
+const createGeneres = () => {
+    let generesAll = [...new Set(peliculas.map(x => x.genero))]
+    generesAll.forEach(item => {
+        generateOption(item, generes);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+   createMovies();
+   createGeneres();
+   createYears();
+   createHtml(peliculas);
+})
+
+
+function generateOption(asign, father){
+    let elementOption = document.createElement('option');
+    elementOption.value = asign;
+    elementOption.textContent = asign;
+    father.appendChild(elementOption);
+}
+
+function createHtml(moviesActually){
+
+
+    clearHtml();
+
+    if(moviesActually.length <= 0){
+        let paragraph = document.createElement('h1');
+        paragraph.textContent = "No results have been found with your search";
+        resultHtml.appendChild(paragraph);
+
+    }else{
+        moviesActually.forEach(item => {
+
+            let cardDiv = document.createElement('div')
+            cardDiv.classList.add('cardStyles')
+            cardDiv.innerHTML =
+            `
+                <h3>${item.nombre}</h3>
+                <h4>Genero: ${item.genero}</h4>
+                <h4>Año: ${item.año}</h4>
+            `
+    
+    
+            resultHtml.appendChild(cardDiv);
+    
+        })
+    }
+
+
+} 
+
+
+
+
+function filterMovies(){
+    let nuevaMovies = peliculas
+    .filter( filterTitle )
+    .filter( filterYear )
+    .filter( filterGenere );
+
+
+    createHtml(nuevaMovies);
+}
+
+
+
+
+
+//Functions filters
+
+function filterTitle(movi){
+    const { nameMovie } = objMovie;
+    return nameMovie ? nameMovie === movi.nombre : movi;
+}
+
+
+function filterYear(movi){
+    const { yearsMovie } = objMovie;
+    return yearsMovie ? parseInt(yearsMovie) === movi.año : movi;
+}
+
+function filterGenere(movi){
+    const { nameGenere } = objMovie;
+    return nameGenere ? nameGenere === movi.genero : movi;
+}
+
+
+
+
+
+
+//Filter each taht change
+
+titles.addEventListener('change', (e) => {
+    objMovie.nameMovie = e.target.value;
+    
+    filterMovies();
+})
+
+
+generes.addEventListener('change', (e) => {
+    objMovie.nameGenere = e.target.value;
+    
+    filterMovies()
+})
+
+
+
+years.addEventListener('change', (e) => {
+    objMovie.yearsMovie = e.target.value;
+    
+    filterMovies()
+})
+
+
+
+
+//Function for clear html of the filters
+
+function clearHtml(){
+    while(resultHtml.firstChild){
+        resultHtml.removeChild(resultHtml.firstChild)
+    }
+}
--- FiltradorBusqueda/js/base.js
+++ FiltradorBusqueda/js/base.js
@@ -1,23 +1,22 @@
-const peliculas = 
+export const peliculas = 
 [
-    {nombre: "The Dark Knight", genero: "Acción", año: 2008},
-    {nombre: "Inception", genero: "Ciencia ficción", año: 2010},
-    {nombre: "Avatar", genero: "Ciencia ficción", año: 2009},
-    {nombre: "The Avengers", genero: "Acción", año: 2012},
+    {nombre: "The Dark Knight", genero: "Action", año: 2008},
+    {nombre: "Inception", genero: "Fiction Sciense", año: 2010},
+    {nombre: "Avatar", genero: "Fiction Sciense", año: 2009},
+    {nombre: "The Avengers", genero: "Action", año: 2012},
     {nombre: "The Social Network", genero: "Drama", año: 2010},
-    {nombre: "Black Panther", genero: "Acción", año: 2018},
-    {nombre: "The Revenant", genero: "Aventura", año: 2015},
+    {nombre: "Black Panther", genero: "Action", año: 2018},
+    {nombre: "The Revenant", genero: "Adventure", año: 2015},
     {nombre: "The Grand Budapest Hotel", genero: "Comedia", año: 2014},
     {nombre: "La La Land", genero: "Musical", año: 2016},
-    {nombre: "Mad Max: Fury Road", genero: "Acción", año: 2015},
-    {nombre: "The Shape of Water", genero: "Fantasía", año: 2017},
-    {nombre: "Interstellar", genero: "Ciencia ficción", año: 2014 },
+    {nombre: "Mad Max: Fury Road", genero: "Action", año: 2015},
+    {nombre: "The Shape of Water", genero: "Fantastic", año: 2017},
+    {nombre: "Interstellar", genero: "Fiction Sciense", año: 2014 },
     { nombre: "Get Out", genero: "Terror", año: 2017 },
     { nombre: "Parasite", genero: "Drama", año: 2019 },
     { nombre: "Birdman", genero: "Drama", año: 2014 },
-    { nombre: "The Martian", genero: "Ciencia ficción", año: 2015 },
-    { nombre: "La La Land", genero: "Musical", año: 2016 },
+    { nombre: "The Martian", genero: "Fiction Sciense", año: 2015 },
     { nombre: "Moonlight", genero: "Drama", año: 2016 },
-    { nombre: "The Shape of Water", genero: "Fantasía", año: 2017 },
-    { nombre: "Inception", genero: "Ciencia ficción", año: 2010 },
-]
+    { nombre: "The Shape of Water", genero: "Fantastic", año: 2017 },
+];
+
