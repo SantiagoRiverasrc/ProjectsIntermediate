6a1609a869f8d1e1701f1230f3c31b256a3c5546
Santino <santiagorvrcn@gmail.com>
2023-10-17 15:07:23 -0500

    Add localStorage

--- TweetLocal/app.js
+++ TweetLocal/app.js
@@ -1,30 +1,90 @@
 const form = document.querySelector(".form_twitter");
 const inp_result = document.querySelector('#inp_twitter');
 const list_result = document.querySelector('#tweets_result');
+const buttonDelete = document.querySelector('.btn_deleteAll');
+const contaBtn = document.querySelector('.container_btn')
 const empty = document.createElement('h3');
+
 empty.textContent = "Empty value, Try again!";
+let miArr;
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
+    
+    
+    if(localStorage.getItem('myArr')){
+        miArr = JSON.parse(localStorage.getItem('myArr'));
+        creatorTemplate(miArr)
+
+    }else{
+        miArr = [];
+    }
+    
+   
+
+    buttonDelete.addEventListener('click', () => {
+        localStorage.removeItem('myArr')
+        list_result.innerHTML = ""
+        miArr = []
+    });
+
+
+    list_result.addEventListener('click', (e)=> {
+        let elemento = e.target.textContent;
+
+        miArr = miArr.filter(x => x !== elemento);
+        localStorage.removeItem('myArr')
+        localStorage.setItem('myArr', JSON.stringify(miArr))
+        list_result.innerHTML = "";
+
+        creatorTemplate(miArr)
+
+
+
+    })
+
+    
+
+
+
+    
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        let _value = inp_result.value.trim()
+        let _value = inp_result.value.trim();
 
+
+        
         if(_value){
-            const tweet = document.createElement('li');
-            tweet.textContent = _value;
-            localStorage.setItem('all', tweet.textContent)
-            tweet.classList.add('li_tweet')
-            list_result.appendChild(tweet);
+            miArr.push(_value)
+            
+            list_result.innerHTML = "";
+            creatorTemplate(miArr);
 
             if(form.childNodes[7] !== undefined){
                 form.lastChild.remove();
             }
-
             
             inp_result.value = "";
         }else{
             form.appendChild(empty);
         }
+        
+        localStorage.setItem('myArr', JSON.stringify(miArr));
+
     })
+
+
+
 })
+
+
+
+
+function creatorTemplate(arr){
+    arr.forEach(item => {
+        const tweet = document.createElement('li');
+        tweet.textContent = item;
+        tweet.classList.add('li_tweet')
+        list_result.appendChild(tweet);
+    });
+}
